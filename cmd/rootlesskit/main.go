47a748ec5675e91349f950c79b2fccf55b86db59
Tobias Schug <happytobi@tscoding.de>
2020-11-04 07:16:18 +0100

    UPDATE create lock on state dir
    Signed-off-by: Tobias Schug <happytobi@tscoding.de>

--- cmd/rootlesskit/main.go
+++ cmd/rootlesskit/main.go
@@ -226,7 +226,6 @@ func createParentOpt(clicontext *cli.Context, pipeFDEnvKey, stateDirEnvKey, pare
 	opt := parent.Opt{
 		PipeFDEnvKey:     pipeFDEnvKey,
 		StateDirEnvKey:   stateDirEnvKey,
-		StateDirTemp:     false,
 		CreatePIDNS:      clicontext.Bool("pidns"),
 		CreateCgroupNS:   clicontext.Bool("cgroupns"),
 		CreateUTSNS:      clicontext.Bool("utsns"),
@@ -241,7 +240,6 @@ func createParentOpt(clicontext *cli.Context, pipeFDEnvKey, stateDirEnvKey, pare
 		if err != nil {
 			return opt, errors.Wrap(err, "creating a state directory")
 		}
-		opt.StateDirTemp = true
 	} else {
 		opt.StateDir, err = filepath.Abs(opt.StateDir)
 		if err != nil {
